79cb778f0ccf8e96d2b3b3ca019ce9b2a9505879
Emilio Soto Andrade <emiliosotoandrade256@gmail.com>
2024-07-22 16:00:25 -0400

    change: url api

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,7 @@
 // dev api url: http://localhost:3000/api
 // prod api url: https://appointment-scheduler-api.onrender.com/api
 
-export const DEV_API_URL = "http://localhost:3000/api"
+export const DEV_API_URL = "https://appointment-scheduler-api.onrender.com/api"
 
 export const ALL_TIMES = [
   '08:30', '08:50', '09:10', '09:30', '09:50',
