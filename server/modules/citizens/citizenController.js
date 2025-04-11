6db31d015be9dd50ce42805a82d68650b6ddfcf3
Emilio Soto Andrade <emiliosotoandrade256@gmail.com>
2025-04-11 12:32:23 -0400

    CHANGE: cambio de base de datos a PostgreSQL

--- server/modules/citizens/citizenController.js
+++ server/modules/citizens/citizenController.js
@@ -18,9 +18,9 @@ export const getAllCitizens = async (req, res) => {
       offset: offset,
       where: search ? {
         [Op.or]: [
-          { nombres: { [Op.like]: `%${search}%` } },
-          { apellidos: { [Op.like]: `%${search}%` } },
-          { rut: { [Op.like]: `%${search}%` } }
+          { nombres: { [Op.iLike]: `%${search}%` } },
+          { apellidos: { [Op.iLike]: `%${search}%` } },
+          { rut: { [Op.iLike]: `%${search}%` } }
         ]
       } : undefined
     })
