a879b3ec5c6f2b9ffd5a29fd39811cbfcb594bda
Emilio Soto Andrade <emiliosotoandrade256@gmail.com>
2025-04-11 12:56:51 -0400

    CHANGE: Cambio en URL de la API para el frontend

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,12 +1,4 @@
 // dev api url: http://localhost:3000/api
 // prod api url: https://appointment-scheduler-api.onrender.com/api
 
-export const DEV_API_URL = "http://localhost:10000/api"
-
-export const ALL_TIMES = [
-  '08:30', '09:00', '09:30', '10:00', '10:30',
-  '11:00', '11:30', '12:00', '12:30', '13:00',
-  '13:20', '13:40', '14:00', '14:20', '14:40',
-  '15:00', '15:20', '15:40', '16:00', '16:20',
-  '16:40', '17:00', '17:20', '17:40', '18:00', '18:20'
-]
+export const DEV_API_URL = "https://sistema-de-audiencias-municipal.onrender.com/api"
